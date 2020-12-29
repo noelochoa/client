ee83ab5b17582de9646813eed12259e9b911f465
noelochoa <35244256+noelochoa@users.noreply.github.com>
2020-12-29 13:37:21 +0900

    extract css

--- quasar.conf.js
+++ quasar.conf.js
@@ -60,7 +60,7 @@ module.exports = function(ctx) {
       // analyze: true,
 
       // Options below are automatically set depending on the env, set them if you want to override
-      // extractCSS: false,
+      extractCSS: true,
 
       // https://quasar.dev/quasar-cli/handling-webpack
       extendWebpack(cfg) {
